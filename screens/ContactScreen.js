5701479a4331051dbbaa86d020d0372b8645a271
Mervyn <limryan@sas.upenn.edu>
2020-12-04 13:08:34 +0800

    edited pictures

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,11 +1,68 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import Ionicons from "react-native-vector-icons/Ionicons";
+
+import React from "react";
+import { View } from "react-native";
+import {
+  Container,
+  Content,
+  Header,
+  List,
+  ListItem,
+  Left,
+  Right,
+  Thumbnail,
+  Body,
+  Text,
+} from "native-base";
+
+const contactsData = [
+  {
+    name: "Ivysaur",
+    title: "#002",
+    company: "Grass | Poison",
+    pic: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png",
+  },
+  {
+    name: "Butterfree",
+    title: "#012",
+    company: "Flying",
+    pic: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png",
+  },
+  {
+    name: "Squirtle",
+    title: "#007",
+    company: "Water",
+    pic: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+  },
+  {
+    name: "Charmeleon",
+    title: "#005",
+    company: "Fire",
+    pic: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png",
+  },
+];
 
 export default function ContactScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Contact!</Text>
-      </View>
-    );
-  }
+  return (
+    <Container>
+      <Content>
+        <List>
+          {contactsData.map((item) => {
+            return (
+              <ListItem avatar>
+                <Left>
+                  <Thumbnail source={{ uri: item.pic }} />
+                </Left>
+                <Body>
+                  <Text>{item.name}</Text>
+                  <Text note>
+                    {item.title}, {item.company}
+                  </Text>
+                </Body>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Content>
+    </Container>
+  );
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,83 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import Ionicons from "react-native-vector-icons/Ionicons";
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+  Text,
+  Card,
+  CardItem,
+  Left,
+  Thumbnail,
+  Body,
+  Button,
+  Icon,
+  Accordion,
+} from "native-base";
 
-export default function HomeScreen() {
+const dataArray = [
+    {
+      title: "What is this?",
+      content: "It's a card game!",
+    },
+    {
+      title: "Who is this by?",
+      content: "People who brought you Pokemon",
+    },
+    {
+      title: "Why is this?",
+      content: "Why not?",
+    },
+  ];
+  
+  function HomeMainScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+      <View style={{ padding: 10 }}>
+        <Text style={styles.titleText}>Pokedex</Text>
+        <Card style={styles.card}>
+          <CardItem>
+            <Body>
+              <Text style={styles.cardTitle}>Coming Soon!</Text>
+              <Text>
+              Discover new brilliance and maximum power in the Pokémon TCG: Sword & Shield—Vivid Voltage expansion. Even more Pokémon V are lining up to battle, including the newly discovered Mythical Pokémon Zarude!
+              </Text>
+            </Body>
+          </CardItem>
+          <CardItem cardBody>
+            <Image
+              source={{
+                uri:
+                  "https://assets.pokemon.com/assets/cms2/img/trading-card-game/series/swsh_series/swsh04/swsh04_logo_169_en.png",
+              }}
+              style={{ height: 200, width: null, flex: 1 }}
+            />
+          </CardItem>
+        </Card>
+  
+        <Accordion style={styles.accordion} dataArray={dataArray} expanded={0} />
       </View>
     );
   }
+  
+  const Stack = createStackNavigator();
+  
+  export default function HomeScreen() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeMainScreen} />
+      </Stack.Navigator>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    titleText: {
+      fontSize: 30,
+      fontWeight: "bold",
+      textAlign: "center",
+      marginTop: 20,
+      marginBottom: 20,
+    },
+    cardTitle: {
+      fontSize: 20,
+      fontWeight: "bold",
+      marginBottom: 10,
+    },
+  });
