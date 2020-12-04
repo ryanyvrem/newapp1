238933fbebff99cda5d260141a03b454a4c91164
Mervyn <limryan@sas.upenn.edu>
2020-12-04 10:30:20 +0800

    added icons

--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function EventsScreen() {
     return (
